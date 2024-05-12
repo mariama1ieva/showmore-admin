6ddecec2eaaa9c4fcb579b17a660fc85671014bf
mariama1ieva <maryamfa@code.edu.az>
2024-05-12 08:54:59 +0400

    add

---
+++ slider/wwwroot/js/blog.js
@@ -0,0 +1,28 @@
+﻿$(function () {
+
+    $(document).on("click", ".show-more button", function () {
+        let skip = parseInt($(".blog-area").children().length);
+       
+        let blogsCount = parseInt($(".blog-area").attr("data-count"));
+        let parentElem = $(".blog-area");
+        let parentElemContent = $(".blog-area").html();
+
+        console.log(parentElemContent)
+        $.ajax({
+            url: `blog/SHowMore?skip=${skip}`,
+            type: 'GET',
+            success: function (res) {
+                parentElemContent += res;
+                $(parentElem).html(parentElemContent);
+                skip = parseInt($(".blog-area").children().length);
+                if (skip >= blogsCount) {
+                    $(".show-more button").addClass("d-none");
+                }
+            }
+        });
+
+
+    })
+
+
+})
